9bfe9c35b8dd4d0c318b814deacd1c73dc85a25f
Jimmy Tan <jimmytan0424@gmail.com>
2024-03-14 11:18:45 -0700

    Changing/Updating data with mutations

--- src/util/http.js
+++ src/util/http.js
@@ -1,4 +1,4 @@
-// Helper function to fetch events, throw error if occurs
+// Helper function to GET request fetch events, throw error if occurs
 export async function fetchEvents({ signal, searchTerm }) {
   let url = 'http://localhost:3000/events';
 
@@ -18,4 +18,45 @@ export async function fetchEvents({ signal, searchTerm }) {
   const { events } = await response.json();
 
   return events;
+}
+
+// POST to http://localhost:3000/events with eventData as body payload
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: 'POST',
+    body: JSON.stringify(eventData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occured while creating the event.');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+// GET to http://localhost:3000/events/images
+export async function fetchSelectableImages({ signal }) {
+  const response = await fetch('http://localhost:3000/events/images', {
+    signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occured while fetching the images.');
+    error.code = response.status;
+    error.info = await response.json();
+
+    throw error;
+  }
+
+  const { images } = await response.json();
+
+  return images;
 }
