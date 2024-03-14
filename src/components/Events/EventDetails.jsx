b77b9ab3aa6168df2cac27628da9ed0bbfa1c76a
Jimmy Tan <jimmytan0424@gmail.com>
2024-03-14 12:41:49 -0700

    Event detail page and delete feature added

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,105 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { fetchEvent, deleteEvent, queryClient } from '../../util/http.js';
 
 import Header from '../Header.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 
 export default function EventDetails() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', { id: id }],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
+  const {
+    mutate,
+    isPending: isMutationPending,
+    isError: isMutationError,
+    error: mutationError,
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      navigate('/events');
+    },
+  });
+
+  function handleDelete(id) {
+    mutate({ id });
+  }
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <LoadingIndicator />
+        <p>Loading event detail...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="Loading Event Details Error"
+        message={
+          error.info?.message || 'Something wrong in fetching event details.'
+        }
+      />
+    );
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    });
+
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          {isMutationError && (
+            <ErrorBlock
+              title="Delete Error"
+              message={
+                mutationError.info?.message ||
+                'Something wrong in deleting event. Try again later'
+              }
+            />
+          )}
+          {!isMutationPending && (
+            <nav>
+              <button onClick={() => handleDelete(data.id)}>Delete</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          )}
+        </header>
+        <div id="event-details-content">
+          <img
+            src={`http://localhost:3000/${data.image}`}
+            alt={`Image of ${data.title}`}
+          />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +108,7 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
+      <article id="event-details">{content}</article>
     </>
   );
 }
--- src/util/http.js
+++ src/util/http.js
@@ -64,3 +64,37 @@ export async function fetchSelectableImages({ signal }) {
 
   return images;
 }
+
+// GET to http://localhost:3000/events/<id>
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+// DELETE to http://localhost:3000/events/<id>
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
